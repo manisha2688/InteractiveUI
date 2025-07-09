1a8513e2b9bc46925b6d9332e392e576f2485eab
Manisha Raman <manisharaman0307@gmail.com>
2025-07-09 23:24:39 +0530

    I added a border, button, body wrapper, etc

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var name = ""
+    
+
     var body: some View {
         VStack {
         
             Text("What is your name?")
                 .font(.title)
         }
+    TextField("Type your name here", text:
+                Value)
+            .multilineTextAlignment(.center)
+            .font(.title)
+            .border(Color.pink, width: 4)
+        Button("Submit name")
+         print(name)
+            .font(.title2)
+            .buttonStyle(
+                .borderedProminent)
+            .tint(.pink)
         .padding()
     }
 }
