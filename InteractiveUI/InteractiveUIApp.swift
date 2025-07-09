d4147490eb5557a45b08d3885c8bd496d9668ddb
Manisha Raman <manisharaman0307@gmail.com>
2025-07-09 22:45:07 +0530

    Initial Commit

---
+++ InteractiveUI/InteractiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIApp.swift
+//  InteractiveUI
+//
+//  Created by Manisha Raman on 9/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
